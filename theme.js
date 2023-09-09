5614629d4aac62cf151a56c3c75aa9ccfdb27eab
zolppy <gabriel.lcifba@gmail.com>
2023-09-09 19:04:48 -0300

    add, commit e push feito por script bash

--- theme.js
+++ theme.js
@@ -1,15 +1,4 @@
-// Time
-const hoursContainer = document.getElementById('hours');
-const minutesContainer = document.getElementById('minutes');
-const secondsContainer = document.getElementById('seconds');
-// Date
-const yearContainer = document.getElementById('year');
-const monthContainer = document.getElementById('month');
-const dayContainer = document.getElementById('day');
-
 const toggleThemeButton = document.getElementById('toggle-theme-button');
-
-const getTimeUnit = unit => unit < 10 ? '0' + unit : unit;
 
 function loadTheme() {
   const bodyEl = document.querySelector('body');
@@ -55,33 +44,7 @@ function toggleTheme() {
   }
 }
 
-const insertDate = () => {
-  const time = new Date();
-  let day = time.getDate();
-  let month = time.getMonth() + 1;
-  let year = time.getFullYear();
-
-  dayContainer.textContent = getTimeUnit(day);
-  monthContainer.textContent = getTimeUnit(month);
-  yearContainer.textContent = getTimeUnit(year);
-}
-
-const updateTime = () => {
-  const time = new Date();
-  let hours = time.getHours();
-  let minutes = time.getMinutes();
-  let seconds = time.getSeconds();
-
-  hoursContainer.textContent = getTimeUnit(hours);
-  minutesContainer.textContent = getTimeUnit(minutes);
-  secondsContainer.textContent = getTimeUnit(seconds);
-}
-
 window.addEventListener('load', () => {
   loadTheme();
-  insertDate();
 });
 toggleThemeButton.addEventListener('click', toggleTheme);
-
-setInterval(updateTime, 1000);
-setInterval(insertDate, (60 * 1000))
